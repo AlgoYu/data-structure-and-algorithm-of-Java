237fe09ed5b232d63a6f01fda89265c849823d4b
AnyDev <794763733@qq.com>
2020-03-21 22:00:17 +0800

    add array cricular queue

--- src/cn/anydevelop/datastructure/linear/ArrayCircularQueue.java
+++ src/cn/anydevelop/datastructure/linear/ArrayCircularQueue.java
@@ -1,16 +1,17 @@
 /*
-队列：队列是一个先进先出的数据结构，大部分情况下用来模拟排队的情况。
+环形队列：环形队列是一个先进先出的数据结构，修复了队列空间重用的问题。
 主要思想：
 	1. 创建一个数组作为队列。
 	2. 使用front作为前指针，使用tail最为尾指针。
 	3. 前、尾指针保存的是队列的下标。
 	4. 通过前、尾指针判断队列是否为空、满、增加数据、删除数据。
+	5. 通过取余算法重置前、尾指针指向的下标。
 */
 package cn.anydevelop.datastructure.linear;
 
 import java.util.Scanner;
 
-public class ArrayQueue {
+public class ArrayCircularQueue {
     // 队列
     private int[] queue;
     // 队列大小
@@ -21,10 +22,10 @@ public class ArrayQueue {
     private int tail;
 
     // 构造函数初始化队列
-    public ArrayQueue(int size) {
+    public ArrayCircularQueue(int size) {
         this.size = size;
         this.queue = new int[size];
-        this.front = this.tail = -1;
+        this.front = this.tail = 0;
     }
 
     // 判断队列是否为空
@@ -34,15 +35,17 @@ public class ArrayQueue {
 
     // 判断队列是否已满
     public boolean isFull(){
-        return this.tail+1 == this.size;
+        return (this.tail+1) % this.size == this.front;
     }
 
     // 数据加入队列尾部
     public void addQueue(int data){
         if(this.isFull()){
-            throw new RuntimeException("Can't add queue because queue is full");
+            System.out.println("Can't add queue because queue is full");
+            return;
         }
-        this.queue[++this.tail] = data;
+        this.queue[this.tail] = data;
+        this.tail = (this.tail+1)%this.size;
     }
 
     // 队列头部数据出队列
@@ -50,13 +53,20 @@ public class ArrayQueue {
         if(this.isEmpty()){
             throw new RuntimeException("Can't add queue because queue is full");
         }
-        return this.queue[++this.front];
+        int data = this.queue[this.front];
+        this.front = (this.front+1) % this.size;
+        return data;
+    }
+
+    // 得到当前队列长度
+    public int getCurrentQueueLength(){
+        return (this.tail+this.size-this.front)%this.size;
     }
 
     // 打印队列
     public void printQueue(){
-        for (int value : this.queue){
-            System.out.print(value+" ");
+        for (int i = this.front;i < this.front+getCurrentQueueLength();i++){
+            System.out.print(this.queue[i%this.size]+" ");
         }
         System.out.println();
     }
@@ -64,15 +74,15 @@ public class ArrayQueue {
     // 打印当前队列头部
     public void printCurrentQueueHead(){
         if(this.isEmpty()){
-            System.out.println("Current have not data");
+            System.out.println("Current not have data");
         }else{
-            System.out.println(this.queue[this.front+1]);
+            System.out.println(this.queue[this.front]);
         }
     }
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        ArrayQueue arrayQueue = new ArrayQueue(5);
+        ArrayCircularQueue arrayCircularQueue = new ArrayCircularQueue(5);
         boolean loop = true;
         while (loop){
             System.out.println("输入1为打印队列");
@@ -83,16 +93,16 @@ public class ArrayQueue {
             int input = scanner.nextInt();
             switch (input){
                 case 1:
-                    arrayQueue.printQueue();
+                    arrayCircularQueue.printQueue();
                     break;
                 case 2:
-                    arrayQueue.addQueue(scanner.nextInt());
+                    arrayCircularQueue.addQueue(scanner.nextInt());
                     break;
                 case 3:
-                    System.out.println(arrayQueue.outQueue());
+                    System.out.println(arrayCircularQueue.outQueue());
                     break;
                 case 4:
-                    arrayQueue.printCurrentQueueHead();
+                    arrayCircularQueue.printCurrentQueueHead();
                     break;
                 case 5:
                     loop = false;
